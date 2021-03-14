5a91eb7d6e5366bcb9ffdaa52f90aef6e39bc74d
Tero Äijälä <tero.aijala@netum.fi>
2021-03-14 20:11:38 +0200

    Fix flake8 complaints

--- app/client.py
+++ app/client.py
@@ -10,8 +10,10 @@ from huawei_lte_api.AuthorizedConnection import AuthorizedConnection
 PARSER = argparse.ArgumentParser("client.py")
 PARSER.add_argument("target_address", help="IP address of the Huawei router")
 PARSER.add_argument("password", help="Admin user password.")
-PARSER.add_argument("--signal", dest="signal", action="store_true", help="Signal status.", required=False)
-PARSER.add_argument("--info", dest="info", action="store_true", help="Device info.", required=False)
+PARSER.add_argument("--signal", dest="signal",
+                    action="store_true", help="Signal status.", required=False)
+PARSER.add_argument("--info", dest="info",
+                    action="store_true", help="Device info.", required=False)
 ARGS = PARSER.parse_args()
 
 URL = "http://admin:" + ARGS.password + "@" + ARGS.target_address
